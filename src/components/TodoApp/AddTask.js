f2cf2fa812a0b29416ba11b7276716766764f260
haristiks <nklharis@gmail.com>
2023-07-17 10:35:56 +0530

    first commit

---
+++ src/components/TodoApp/AddTask.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function AddTask({ addTask }) {
+  const [value, setValue] = useState("");
+
+  const addItem = () => {
+    addTask(value);
+    setValue("");
+  };
+  return (
+    <>
+      <div className="input-container">
+        <input
+          type="text"
+          className="input"
+          placeholder="Add a new Task"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button onClick={addItem} className="add-btn">
+          ADD
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default AddTask;
---
+++ src/components/TodoApp/Todo.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import "./Todo.css";
+import ListTask from "./ListTask";
+import AddTask from "./AddTask";
+import { useState } from "react";
+
+function Todo() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    document.title = `You have ${tasks.length} pending tasks(s)`;
+  });
+
+  const addTask = (title) => {
+    const newTask = [...tasks, { title }];
+    setTasks(newTask);
+  };
+
+  const removeTask = (index) => {
+    const newTask = [...tasks];
+    newTask.splice(index, 1);
+    setTasks(newTask);
+  };
+  return (
+    <>
+      <div className="todo-container">
+        <div className="header">TODO APP</div>
+        <div className="add-task">
+          <AddTask addTask={addTask} />
+        </div>
+        <div className="tasks">
+          {tasks.map((task, index) => (
+            <ListTask
+              task={task}
+              removeTask={removeTask}
+              index={index}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Todo;
