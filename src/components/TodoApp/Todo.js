f2e1e027f3a92bbfbd439b8f5be8c90d0b7401ea
haristiks <nklharis@gmail.com>
2023-07-20 13:24:05 +0530

    nav bar added

--- src/components/TodoApp/Todo.js
+++ src/components/TodoApp/Todo.js
@@ -3,6 +3,9 @@ import "./Todo.css";
 import ListTask from "./ListTask";
 import AddTask from "./AddTask";
 import { useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Todo() {
   const [tasks, setTasks] = useState([]);
@@ -22,6 +25,20 @@ function Todo() {
   };
   return (
     <>
+      <div>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">TODO APP</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#tasks">Tasks</Nav.Link>
+            <Nav.Link href="#reminder">Remainder</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
+      </div>
+
       <div className="todo-container">
         <div className="header">TODO APP</div>
         <div className="add-task">
